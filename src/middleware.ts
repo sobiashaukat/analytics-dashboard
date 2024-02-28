f0d8eadf6fc21c84a7401543a1d1709ab428fe05
sobiashaukat <126345014+sobiashaukat@users.noreply.github.com>
2024-02-28 22:51:15 +0500

    final push

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,19 +2,20 @@ import { NextRequest, NextResponse } from "next/server";
 import { analytics } from "@/utils/analytics";
 
 export default async function middleware(req: NextRequest) {
-  if (req.nextUrl.pathname === "/") {
+  if (req.nextUrl.pathname === '/') {
     try {
-      analytics.track("pageview", {
-        page: "/",
+      analytics.track('pageview', {
+        page: '/',
         country: req.geo?.country,
-      });
+        name:"john"
+      })
     } catch (err) {
-      console.error(err);
+      console.error(err)
     }
   }
-  return NextResponse.next();
+  return NextResponse.next()
 }
 
 export const matcher = {
-  matcher: ["/"],
+  matcher: ['/'],
 };
